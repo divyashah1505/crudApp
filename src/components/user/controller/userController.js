8484b60c0981b3656624bf5e6059c9604e8bddf9
divyashah1505 <156400689+divyashah1505@users.noreply.github.com>
2026-01-29 13:42:15 +0530

    Update userController.js

--- src/components/user/controller/userController.js
+++ src/components/user/controller/userController.js
@@ -1,122 +1,198 @@
 const User = require("../model/users");
+const Address = require("../../address/model/address");
 const { generateTokens, success, error } = require("../../utils/commonUtils");
 const { appString } = require("../../utils/appString");
-// const appappString = require("../../utils/appString");
 
 const userController = {
+
+  // ===================== REGISTER =====================
   register: async (req, res) => {
-
-    // console.log("bbbbbb");
-    
     try {
-      const {username,email,password,file}=req.body;
-      
+      const { username, email, password, file } = req.body;
 
-      const user = await User.create({username,email,password,file});
+      const user = await User.create({ username, email, password, file });
       const tokens = generateTokens(user._id);
+
       return success(res, { user, ...tokens }, appString.USER_CREATED, 201);
     } catch (err) {
       if (err.code === 11000) {
         const field = Object.keys(err.keyValue)[0];
         return error(res, `${field} already exists`, 409);
       }
-      return error(res, err.message || appString.REGISTRATION_FAILED, 400);
-    }
-  },
-  
-   insertAddress:async(req,res)=>{
-    try{
-      const {street,city,state,zipCode,country}=req.body;
-      const user = await userAddress.create({street,city,state,zipCode,country});
-      return success(res,{user},appString.ADDRESS_CREATED,201);
-    }catch (err){
-       return error(res,)
-    }
-  },
-  profileUpload: async (req, res) => {
-    try {
-
-      if (req.files) {
-        success(res,  appString.USER_FILE_UPLOADED);
-      } else {
-        error(res, appString.USER_FILE_INVALID, 404);
-      }
-    } catch (err) {
-      error( res, err.message, 500);
+      return error(res, err.message, 400);
     }
   },
 
+  // ===================== LOGIN =====================
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
-      
+
       if (!email || !password) {
         return error(res, appString.Required_EmailPass, 400);
       }
 
       const user = await User.findOne({ email });
-
       if (!user || !(await user.matchPassword(password))) {
         return error(res, appString.INVALID_CREDENTIALS, 401);
       }
 
       const tokens = generateTokens(user._id);
 
-      success(
-        res,
-        {
-          username: user.username,
-          email: user.email,
-          accessToken: tokens.accessToken,
-          refreshToken: tokens.refreshToken,
-        },
-        appString.LOGIN_SUCCESS,
-      );
+      return success(res, {
+        username: user.username,
+        email: user.email,
+        ...tokens,
+      }, appString.LOGIN_SUCCESS);
     } catch (err) {
-      error(res, err.message || appString.LOGIN_FAILED, 500);
+      return error(res, err.message, 500);
     }
   },
 
-  getProfile: async (req, res) => {
+  // ===================== INSERT ADDRESS =====================
+  insertAddress: async (req, res) => {
     try {
-      const user = await User.findById(req?.user?.id).select("username email");
+      const userId = req.user.id;
+      const { street, city, state, zipCode, country, isPrimary } = req.body;
 
-      if (!user) {
+      if (isPrimary) {
+        await Address.updateMany(
+          { userId, isPrimary: true },
+          { isPrimary: false }
+        );
+      }
+
+      const address = await Address.create({
+        userId,
+        street,
+        city,
+        state,
+        zipCode,
+        country,
+        isPrimary: !!isPrimary,
+      });
+
+      if (isPrimary) {
+        await User.findByIdAndUpdate(userId, {
+          primaryAddress: address._id,
+        });
+      }
+
+      return success(res, address, appString.ADDRESS_CREATED, 201);
+    } catch (err) {
+      return error(res, err.message, 400);
+    }
+  },
+
+  // ===================== CHANGE PRIMARY ADDRESS =====================
+  changePrimaryAddress: async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const { addressId } = req.body;
+
+      await Address.updateMany(
+        { userId, isPrimary: true },
+        { isPrimary: false }
+      );
+
+      const address = await Address.findOneAndUpdate(
+        { _id: addressId, userId },
+        { isPrimary: true },
+        { new: true }
+      );
+
+      if (!address) {
         return error(res, appString.NOT_FOUND, 404);
       }
-      success(res, user);
+
+      await User.findByIdAndUpdate(userId, {
+        primaryAddress: address._id,
+      });
+
+      return success(res, address, appString.PRIMARY_ADDRESS_UPDATED);
     } catch (err) {
-      error(res, err.message, 500);
+      return error(res, err.message, 400);
     }
   },
 
-  updateUser: async (req, res) => {
+  // ===================== LIST USER ADDRESSES =====================
+  listUserAddresses: async (req, res) => {
     try {
-      const user = await User.findByIdAndUpdate(req?.user?.id, req.body, {
-        new: true,
-      });
-      success(res, user, appString.USER_UPDATED);
+      const addresses = await Address.find({ userId: req.user.id });
+      return success(res, addresses);
     } catch (err) {
-      error(req, res, err.message, 400);
+      return error(res, err.message, 500);
     }
   },
- 
+
+  // ===================== GET PROFILE (PRIMARY ADDRESS ONLY) =====================
+  getProfile: async (req, res) => {
+    try {
+      const userId = req.user.id;
+
+      const profile = await User.aggregate([
+        { $match: { _id: userId } },
+        {
+          $lookup: {
+            from: "addresses",
+            let: { primaryId: "$primaryAddress" },
+            pipeline: [
+              {
+                $match: {
+                  $expr: { $eq: ["$_id", "$$primaryId"] },
+                },
+              },
+            ],
+            as: "primaryAddress",
+          },
+        },
+        {
+          $unwind: {
+            path: "$primaryAddress",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $project: {
+            username: 1,
+            email: 1,
+            primaryAddress: 1,
+          },
+        },
+      ]);
+
+      if (!profile.length) {
+        return error(res, appString.NOT_FOUND, 404);
+      }
+
+      return success(res, profile[0]);
+    } catch (err) {
+      return error(res, err.message, 500);
+    }
+  },
+
+  // ===================== UPDATE USER =====================
+  updateUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.user.id,
+        req.body,
+        { new: true }
+      );
+      return success(res, user, appString.USER_UPDATED);
+    } catch (err) {
+      return error(res, err.message, 400);
+    }
+  },
+
+  // ===================== DELETE USER =====================
   deleteUser: async (req, res) => {
     try {
       await User.softDelete(req.user.id);
-      success(req, res, {}, appString.USER_DELETED);
+      return success(res, {}, appString.USER_DELETED);
     } catch (err) {
-      error(res, err.message, 400);
+      return error(res, err.message, 400);
     }
-  },
-  imgUpload: async (req, res) => {},
-
-  logout: (req, res) => {
-    res.clearCookie("accessToken");
-    res.status(200).json({
-      success: true,
-      message: appString.LOGOUT_SUCCESS,
-    });
   },
 };
 
